788a7ec645a45daa7c899cc4ee38bb93bf3f7cd6
leotrimm221 <130552711+leotrimm221@users.noreply.github.com>
2023-05-05 14:51:36 +0200

    Add files via upload

---
+++ frmProducts.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.Ul
+{
+    public partial class frmProducts : Form
+    {
+        public frmProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void lblTop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //Add code to hide this form
+            this.Hide();
+        }
+    }
+}
