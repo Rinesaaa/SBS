f24b85834532ca831ca3df7cf4aab145ba9b508a
leotrimm221 <130552711+leotrimm221@users.noreply.github.com>
2023-05-05 14:22:18 +0200

    Add files via upload

---
+++ frmLogin.cs
@@ -0,0 +1,83 @@
+﻿using AnyStore.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.Ul
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        loginBILLL l = new loginBILLL();
+        loginDAL dal = new loginDAL();
+        public static string loggedIn;
+        
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pboxClose_Click(object sender, EventArgs e)
+        {
+            //code to close this fore 
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            l.username = txtUsername.Text.Trim();
+            l.password = txtPassword.Text.Trim();
+            l.user_type = cmbUserType.Text.Trim();
+
+            bool success = dal.LoginCheck(l);
+            if (success == true)
+            {
+                MessageBox.Show("Login Successfull.");
+                loggedIn = l.username;
+                switch (l.user_type)
+                {
+                    case "Admin":
+                        {
+                            frmAdminDashboard admin = new frmAdminDashboard();
+                            admin.Show();
+                            this.Hide();
+                        }
+                        break;
+
+                    case "User":
+                        {
+                            frmUserDashboard user = new frmUserDashboard();
+                            user.Show();
+                            this.Hide();
+                        }
+                        break;
+
+                    default:
+                        {
+                            MessageBox.Show("Invalid User Type.");
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Login Failed. Try Again");
+            }
+        }
+    }
+}
