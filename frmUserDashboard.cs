0f0280ebecaf2f98638fb12abf49fb987e65fd6a
Rinesaaa <93437204+Rinesaaa@users.noreply.github.com>
2023-05-05 14:18:25 +0200

    Add files via upload

---
+++ frmUserDashboard.cs
@@ -0,0 +1,37 @@
+﻿using AnyStore.Ul;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore
+{
+    public partial class frmUserDashboard : Form
+    {
+        public frmUserDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void lblLoggedInUser_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void frmUserDashboard_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            frmLogin login = new frmLogin();
+            login.Show();
+            this.Hide();
+        }
+
+        private void frmUserDashboard_Load(object sender, EventArgs e)
+        {
+            lblLoggedInUser.Text = frmLogin.loggedIn;
+        }
+    }
+}
