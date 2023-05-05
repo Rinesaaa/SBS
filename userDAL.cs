724319932b5824738913e933f7644794bbd98ae9
Rinesaaa <93437204+Rinesaaa@users.noreply.github.com>
2023-05-05 14:16:35 +0200

    Add files via upload

---
+++ userDAL.cs
@@ -0,0 +1,257 @@
+﻿using AnyStore.BILLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.DAL
+{
+    internal class UserDAL
+    {
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;       
+
+        #region Select Data from Database
+        public DataTable Select()
+        {
+            //static method to connect databas
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            //to hold the data from database
+            DataTable dt = new DataTable();
+            try
+            {
+                //SQL Query to Get Data From Database
+                String sql = "SELECT * FROM tbl_users";
+                //for Executing command
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //getting data from database
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                //database connection open
+                conn.Open();
+                //fill data in our dataTable
+                adapter.Fill(dt);
+            }
+            catch(Exception ex)
+            {
+                //throw message if any error occurs
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //closing connection
+                conn.Close();
+            }
+            //return the value in dataTable
+            return dt;
+              }
+        #endregion
+        #region Insert Data in Database
+        public bool Insert(UserBLL u)
+        {
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                string sql = "INSERT INTO tbl_users(first_name, last_name, email, username, password, contact, address, gender, user_type, added_date, added_by) VALUES(@first_name, @last_name, @email, @username, @password, @contact, @address, @gender, @user_type, @added_date, @added_by)";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@first_name", u.first_name);
+                cmd.Parameters.AddWithValue("@last_name", u.last_name);
+                cmd.Parameters.AddWithValue("@email", u.email);
+                cmd.Parameters.AddWithValue("@username", u.username);
+                cmd.Parameters.AddWithValue("@password", u.password);
+                cmd.Parameters.AddWithValue("@contact", u.contact);
+                cmd.Parameters.AddWithValue("@address", u.address);
+                cmd.Parameters.AddWithValue("@gender", u.gender);
+                cmd.Parameters.AddWithValue("@user_type", u.user_type);
+                cmd.Parameters.AddWithValue("@added_date", u.added_date);
+                cmd.Parameters.AddWithValue("@added_by", u.added_by);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                //if the query is executed successfully then the value to rows will be greater than 0 else it will be less than 0
+            if(rows>0)
+                {
+                    //query sucessfull
+                    isSuccess = true;
+                }
+            else
+                {
+                    //query failed 
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+        #endregion
+        #region Update data in Database
+        public bool Update(UserBLL u)
+        {
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                string sql = "UPDATE tbl_users SET first_name=@first_name, last_name=@last_name, email=@email, username=@username, password=@password, contact=@contact, address=@address, gender=@gender, user_type=@user_type, added_date=@added_date, added_by=@added_by WHERE id=@id";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@first_name", u.first_name);
+                cmd.Parameters.AddWithValue("@last_name", u.last_name);
+                cmd.Parameters.AddWithValue("@email", u.email);
+                cmd.Parameters.AddWithValue("@username", u.username);
+                cmd.Parameters.AddWithValue("@password", u.password);
+                cmd.Parameters.AddWithValue("@contact", u.contact);
+                cmd.Parameters.AddWithValue("@address", u.address);
+                cmd.Parameters.AddWithValue("@gender", u.gender);
+                cmd.Parameters.AddWithValue("@user_type", u.user_type);
+                cmd.Parameters.AddWithValue("@added_date", u.added_date);
+                cmd.Parameters.AddWithValue("@added_by", u.added_by);
+                cmd.Parameters.AddWithValue("@id", u.id);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    //query successfull
+                    isSuccess = true;
+                }
+                else
+                {
+                    //query faild
+                    isSuccess = false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();  
+            }
+            return isSuccess;
+        }
+        #endregion
+        #region Delete Data from Database
+        public bool Delete(UserBLL u)
+        {
+            bool isSucces = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                string sql = "DELETE FROM tbl_users WHERE id=@id";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@id", u.id);
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                if(rows>0)
+                {
+                    //query successfull
+                    isSucces = true;
+                }
+                else
+                {
+                    //query failed
+                    isSucces = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSucces;
+        }
+
+        #endregion
+        #region Search User on Database usingKeywords
+        public DataTable Search(string keywords)
+        {
+            //static method to connect databas
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            //to hold the data from database
+            DataTable dt = new DataTable();
+            try
+            {
+                //SQL Query to Get Data From Database 
+                String sql = "SELECT * FROM tbl_users WHERE id LIKE '%"+keywords+"%' OR first_name LIKE '%"+keywords+"%' OR last_name LIKE '%"+keywords+ "%' OR username LIKE '%"+keywords+"%'";
+                //for Executing command
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //getting data from database
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                //database connection open
+                conn.Open();
+                //fill data in our dataTable
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                //throw message if any error occurs
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //closing connection
+                conn.Close();
+            }
+            //return the value in dataTable
+            return dt;
+        }
+        #endregion
+        #region Getting User ID from Username
+        public UserBLL GetIdFromUsername (string username)
+        {
+            UserBLL u = new UserBLL();
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string sql = "SELECT id FROM tbl_users WHERE username='" + username + "'";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+                conn.Open();
+
+                adapter.Fill(dt);
+                if(dt.Rows.Count>0)
+                {
+                    u.id = int.Parse(dt.Rows[0]["id"].ToString());
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return u;
+        }
+        #endregion
+    }
+}
+  
