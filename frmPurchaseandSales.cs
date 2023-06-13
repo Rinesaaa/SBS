78a1f335a5bb669b2c4b5cc763357feb1b6e4cc2
zizaab <124061589+zizaab@users.noreply.github.com>
2023-06-13 12:08:31 +0200

    Add files via upload

---
+++ frmPurchaseandSales.cs
@@ -0,0 +1,298 @@
+﻿using AnyStore.BILLL;
+using AnyStore.BLL;
+using AnyStore.DAL;
+using DGVPrinterHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Windows.Forms;
+
+namespace AnyStore.Ul
+{
+    public partial class frmPurchaseandSales : Form
+    {
+        public frmPurchaseandSales()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+        DeaCustDAL dcDAL = new DeaCustDAL();
+        productsDAL pDAL = new productsDAL();
+        userDAL uDAL = new userDAL();
+        transactionDAL tDAL = new transactionDAL();
+        transactionDetailDAL tdDAL = new transactionDetailDAL();
+
+
+        DataTable transactionDT = new DataTable();
+        private productsDAL pDal;
+        private object txtProductName;
+       // private bool tdDAL;
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void txtAddress_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlCalculations_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void frmPurchaseandSales_Load(object sender, EventArgs e)
+        {
+            string type = frmUserDashboard.transactionType;
+            lblTop.Text = type;
+
+            transactionDT.Columns.Add("Product Name");
+            transactionDT.Columns.Add("Rate");
+            transactionDT.Columns.Add("Qty");
+            transactionDT.Columns.Add("Total");
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtSearchProduct_TextChanged(object sender, EventArgs e)
+        {
+            string keyword = txtSearchProduct.Text;
+
+            if (keyword == "")
+            {
+                txtSearchProduct.Text = "";
+                txtInventory.Text = "";
+                txtRate.Text = "";
+                txtQty.Text = "";
+                return;
+            }
+            productsBLL p = pDAL.GetProductsForTransaction(keyword);
+
+            txtNameProduct.Text = p.name;
+            txtInventory.Text = p.qty.ToString();
+         txtRate.Text = p.rate.ToString();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string ProductName = txtNameProduct.Text;
+            decimal Rate = decimal.Parse(txtRate.Text);
+            decimal Qty = decimal.Parse(txtQty.Text);
+
+            decimal Total = Rate * Qty;
+
+            decimal subTotal = decimal.Parse(txtSubTotal.Text);
+            subTotal = subTotal + Total;
+
+            if (ProductName == "")
+            {
+                MessageBox.Show("Select the product first.Try Again.");
+            }
+            else
+            {
+                transactionDT.Rows.Add(ProductName, Rate, Qty, Total);
+
+                dgvAddedProducts.DataSource = transactionDT;
+
+                txtSubTotal.Text = subTotal.ToString();
+
+                txtSearchProduct.Text = "";
+                txtNameProduct.Text = "";
+                txtInventory.Text = "0.00";
+                txtRate.Text = "0.00";
+                txtQty.Text = "0.00";
+            }
+
+        }
+
+        private void txtDiscount_TextChanged(object sender, EventArgs e)
+        {
+            string value = txtDiscount.Text;
+
+            if (value=="")
+            {
+                MessageBox.Show("Please Add Discount Fisrt");
+            }
+            else
+            {
+                decimal subTotal = decimal.Parse(txtSubTotal.Text);
+                decimal discount = decimal.Parse(txtDiscount.Text);
+
+                decimal grandTotal = ((100 - discount) / 100) * subTotal;
+
+                txtGrandTotal.Text = grandTotal.ToString();
+            }
+        }
+
+        private void txtVat_TextChanged(object sender, EventArgs e)
+        {
+            string check = txtGrandTotal.Text;
+            if (check == "")
+            {
+                MessageBox.Show("Calculate the discount and set the Grand Total First.");
+
+            }
+            else
+            {
+                decimal previousGT = decimal.Parse(txtGrandTotal.Text);
+                decimal vat = decimal.Parse(txtVat.Text);
+                decimal grandTotalWithVat = ((100 + vat) / 100) * previousGT;
+
+                txtGrandTotal.Text = grandTotalWithVat.ToString();
+            }
+        }
+
+        private void txtPaidAmount_TextChanged(object sender, EventArgs e)
+        {
+            decimal grandTotal = decimal.Parse(txtGrandTotal.Text);
+            decimal paidamount = decimal.Parse(txtPaidAmount.Text);
+
+            decimal returnamount = paidamount - grandTotal;
+
+            txtReturnAmount.Text = returnamount.ToString();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            TransactionBLL transaction = new TransactionBLL();
+
+            transaction.type = lblTop.Text;
+
+            string deaCustName = txtName.Text;
+            DeaCustBLL dc = dcDAL.GetDeaCustIDFromName(deaCustName);
+
+            transaction.dea_cust_id = dc.id;
+            transaction.grandTotal = Math.Round(decimal.Parse(txtGrandTotal.Text),2);
+            transaction.transaction_date = DateTime.Now;
+            transaction.tax = decimal.Parse(txtVat.Text);
+            transaction.discount = decimal.Parse(txtDiscount.Text);
+
+            string username = frmLogin.loggedIn;
+            UserBLL u = uDAL.GetIdFromUsername(username);
+
+            transaction.added_by = u.id;
+            transaction.TransactionDetail = transactionDT;
+            bool success = false;
+
+            using(TransactionScope scope = new TransactionScope())
+            {
+                int transactionID = -1;
+
+                bool w = tDAL.Insert_Transaction(transaction, out transactionID);
+
+                for (int i = 0; i < transactionDT.Rows.Count; i++)
+                {
+                    TransactionDetailBLL transactionDetail = new TransactionDetailBLL();
+
+                    string ProductName = transactionDT.Rows[i][0].ToString();
+                    productsBLL p = pDAL.GetProductIDFromName(ProductName);
+
+                    transactionDetail.product_id = p.id;
+                    transactionDetail.rate = decimal.Parse(transactionDT.Rows[1][1].ToString());
+                    transactionDetail.qty = decimal.Parse(transactionDT.Rows[1][2].ToString());
+                    transactionDetail.total = Math.Round(decimal.Parse(transactionDT.Rows[1][3].ToString()), 2);
+                    transactionDetail.dea_cust_id = dc.id;
+                    transactionDetail.added_date = DateTime.Now;
+                    transactionDetail.added_by = u.id;
+
+                    string transactionType = lblTop.Text;
+                    bool x = false;
+                    if(transactionType =="Purchase")
+                    {
+                        x = pDAL.IncreaseProduct(transactionDetail.product_id, transactionDetail.qty);
+                 
+                    }
+                    else if(transactionType=="Sales")
+                    {
+                         x = pDAL.DecreaseProduct(transactionDetail.product_id, transactionDetail.qty);
+                    }
+
+                    bool y = tdDAL.InsertTransactionDetail(transactionDetail);
+                    success = w && x && y;
+                }
+
+
+                    if (success == true)
+                        {
+                    scope.Complete();
+
+
+                    DGVPrinter printer = new DGVPrinter();
+                    printer.Title = "\r\n\r\n\r\n ANYSTORE PVT. LTD.  \r\n\r\n";
+                    printer.SubTitle = "Loni Doors, Fushe Kosova \r\n Phone:045XXXXXXX";
+                    printer.SubTitleFormatFlags = StringFormatFlags.LineLimit | StringFormatFlags.NoClip;
+                    printer.PageNumbers = true;
+                    printer.PageNumberInHeader = false;
+                    printer.PorportionalColumns = true;
+                    printer.HeaderCellAlignment = StringAlignment.Near;
+                    printer.Footer = "Discount:" + txtDiscount.Text + "% \r\n" + "VAT:" + txtVat.Text + "% \r\n" + "Grand Total:"+txtGrandTotal.Text + "\r\n" + "Thank you for doing business with us";
+                    printer.FooterSpacing = 15;
+                    printer.PrintDataGridView(dgvAddedProducts);
+
+
+
+
+
+
+
+                            MessageBox.Show("Transaction Completed Successfully");
+
+                            dgvAddedProducts.DataSource = null;
+                            dgvAddedProducts.Rows.Clear();
+
+                            txtSearch.Text = "";
+                            txtName.Text = "";
+                            txtEmail.Text = "";
+                            txtContact.Text = "";
+                            txtAddress.Text = "";
+                            txtSearchProduct.Text = "";
+                            txtNameProduct.Text = "";
+                            txtInventory.Text = "0";
+                            txtRate.Text = "0";
+                            txtQty.Text = "0";
+                            txtSubTotal.Text = "0";
+                            txtDiscount.Text = "0";
+                            txtVat.Text = "0";
+                            txtGrandTotal.Text = "0";
+                            txtPaidAmount.Text = "0";
+                            txtReturnAmount.Text = "0";
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Transaction failed");
+                        }
+
+
+                    }
+
+
+                }
+            }
+       
+        
+        
+        }
+
+    
+
---
+++ productsDAL.cs
@@ -0,0 +1,476 @@
+﻿using AnyStore.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.DAL
+{
+    class productsDAL
+    {
+        //creating stati string method for db connection 
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+        internal object rate;
+        internal object qty;
+        internal string name;
+        #region Select method for Product Module
+        public DataTable Select()
+        {
+            //create sql connection to connect database
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            //datatable to hold  the data from database 
+            DataTable dt = new DataTable();
+
+            try
+            {
+                //writing the query to select all the products from database
+                String sql = "SELECT * FROM tbl_products";
+
+                //creating sql command to execute query 
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //sql data adapter to hold the value from database temporaily
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                //open database connection 
+                conn.Open();
+
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+        }
+        #endregion
+        #region Method to Insert Product in database
+        public bool Insert(productsBLL p)
+        {
+            //creating boolean variable and set its default value to false
+            bool isSuccess = false;
+
+            //sql connection for database
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //sql query to insert  product into database
+                String sql = "INSERT INTO tbl_products (name, category, description, rate, qty, added_date, added_by) VALUES (@name, @category, @description, @rate, @qty, @added_date, @added_by) ";
+
+                //creating sql command to pass the values
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //passign the values through parameters
+                cmd.Parameters.AddWithValue("@name", p.name);
+                cmd.Parameters.AddWithValue("@category", p.category);
+                cmd.Parameters.AddWithValue("@description", p.description);
+                cmd.Parameters.AddWithValue("@rate", p.rate);
+                cmd.Parameters.AddWithValue("@qty", p.qty);
+                cmd.Parameters.AddWithValue("@added_date", p.added_date);
+                cmd.Parameters.AddWithValue("@added_by", p.added_by);
+
+                //opening the database connection 
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                //if the query is executed successfully then the value of rows will be greater than 0 else it will be less than 0
+                if (rows > 0)
+                {
+                    //query executed successfully
+                    isSuccess = true;
+                }
+                else
+                {
+                    //failed to execute query
+                    isSuccess = false;
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+
+
+        #endregion
+        #region Method to Update Products in Database
+        public bool Update(productsBLL p)
+        {
+            //create a boolean variable and set its initail value to false
+            bool isSuccess = false;
+
+            //create sql connection for database
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //sql query to update data in database
+                String sql = "UPDATE tbl_products SER name=@name, category=@category,description=@description, rate=@rate, added_date=@added_date,added_by=@added_by WHERE id=@id";
+                //create sqlcommand to pass the value to query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //passing the value using parameters and cmd
+                cmd.Parameters.AddWithValue("@name", p.name);
+                cmd.Parameters.AddWithValue("@category", p.category);
+                cmd.Parameters.AddWithValue("@description", p.description);
+                cmd.Parameters.AddWithValue("@rate", p.rate);
+                cmd.Parameters.AddWithValue("@qty", p.qty);
+                cmd.Parameters.AddWithValue("@added_date", p.added_date);
+                cmd.Parameters.AddWithValue("@added_by", p.added_by);
+                cmd.Parameters.AddWithValue("@id", p.id);
+                //open the database connection 
+                conn.Open();
+
+                //create int variable to check if the query is executed successfully or not
+                int rows = cmd.ExecuteNonQuery();
+                //if the query is executed successfully then the value of rows will be greater than 0 else it will be less than zero
+                if (rows > 0)
+                {
+                    //query executedSuccessfully
+                    isSuccess = true;
+                }
+                else
+                {
+                    //failed to execute query
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+        #endregion
+        #region Method to Delete Product from database
+        public bool Delete(productsBLL p)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                String sql = "DELETE FROM tbl_products WHERE id=@id";
+
+
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@id", p.id);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    //query executed succesfully 
+                    isSuccess = true;
+                }
+                else
+                {
+                    //failed to execute query
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+
+        public static implicit operator DateTime(productsDAL v)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region SEARCH Method for Product Module
+        public DataTable Search(string keywords)
+        {
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string sql = "SELECT * FROM tbl_products WHERE id LIKE '%" + keywords + "%' OR name LIKE '%" + keywords + "%' OR category LIKE '%" + keywords + "%'";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                //open database connection
+                conn.Open();
+
+                adapter.Fill(dt);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+                }
+
+        #endregion
+        #region Method to Search Product in Transaction Module
+        public productsBLL GetProductsForTransaction(string keyword)
+        {
+            productsBLL p = new productsBLL();
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = "SELECT name,rate,qty FROM tbl_products WHERE id LIKE '%" + keyword + "%' OR name LIKE '%" + keyword + "%'";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+                conn.Open();
+                adapter.Fill(dt);
+
+                if(dt.Rows.Count > 0)
+                {
+                    p.name = dt.Rows[0]["name"].ToString();
+                    p.rate =decimal.Parse( dt.Rows[0]["rate"].ToString());
+                    p.qty = decimal.Parse(dt.Rows[0]["qty"].ToString());
+                }
+            
+            
+            
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return p;
+        }
+        #endregion
+        #region Method to get Product ID based on Product Name
+        public productsBLL GetProductIDFromName(string ProductName)
+        {
+            productsBLL p = new productsBLL();
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = "SELECT id FROM tbl_products WHERE name='" + ProductName + "'";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+                conn.Open();
+                adapter.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    p.id = int.Parse(dt.Rows[0]["id"].ToString());
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return p;
+        }
+        #endregion
+        #region METHOD to get current quantity from the database based on Product ID
+        public decimal GetProductQty(int ProductID)
+        {
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            decimal qty = 0;
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = "SELECT qty FROM tbl_products WHERE id = " + ProductID;
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                conn.Open();
+                adapter.Fill(dt);
+
+                if(dt.Rows.Count>0)
+                {
+                    qty = decimal.Parse(dt.Rows[0]["qty"].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return qty;
+
+
+        }
+
+        #endregion
+        #region  Method to update quantity
+        public bool UpdateQuantity(int ProductID,decimal Qty)
+        {
+            bool success = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                string sql = "UPDATE tbl_products SET qty=@qty WHERE id=@id";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@qty", Qty);
+                cmd.Parameters.AddWithValue("@id", ProductID);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                if(rows>0)
+                {
+                    success = true;
+
+                }
+                else
+                {
+                    success = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return success;
+        }
+        #endregion
+        #region Method to increase product
+        public bool IncreaseProduct(int ProductID, decimal IncreaseQty)
+        {
+            bool success = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                decimal currentQty = GetProductQty(ProductID);
+
+                decimal NewQty = currentQty + IncreaseQty;
+
+                success = UpdateQuantity(ProductID, NewQty);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return success;
+        }
+        #endregion
+        #region Method to decrease product
+        public bool DecreaseProduct(int ProductID, decimal Qty)
+        {
+            bool success = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                decimal currentQty = GetProductQty(ProductID);
+
+                decimal NewQty = currentQty - Qty;
+
+                success = UpdateQuantity(ProductID, NewQty);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return success;
+        }
+        #endregion
+        #region Display products based on categories
+        public DataTable DisplayProdutcsByCategory(string category)
+        {
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string sql ="SELECT * FROM tbl_products WHERE category ='"+category+"'";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+               
+               
+                
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                conn.Open();
+                adapter.Fill(dt);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+        }
+        #endregion
+
+    }
+}
